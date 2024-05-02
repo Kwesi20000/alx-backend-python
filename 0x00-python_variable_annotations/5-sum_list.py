746d53b54e4f71786df22972c715e08662ea97af
Kwesi20000 <hendrickskumi@yahoo.com>
2024-05-02 14:12:43 +0000

    hendy

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""This module contains a function to compute the sum of a list of floats."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Compute the sum of a list of floats and return it."""
+    return sum(input_list)
