b077bd113c020119265197969e758e3e3e8a71c4
Kwesi20000 <hendrickskumi@yahoo.com>
2024-05-02 14:10:13 +0000

    hendy

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""This module contains a function to convert
+a float to its string representation."""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to its string representation and return it."""
+    return str(n)
