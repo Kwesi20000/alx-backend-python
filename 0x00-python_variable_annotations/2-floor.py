a18becedd6424633b3127f683d0a134f91b05e9b
Kwesi20000 <hendrickskumi@yahoo.com>
2024-05-02 14:07:53 +0000

    hendy

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""This module contains a function to compute the floor of a float."""
+
+
+def floor(n: float) -> int:
+    """Compute the floor of a float and return it."""
+    return int(n // 1)
